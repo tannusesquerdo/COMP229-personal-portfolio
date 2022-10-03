470f15f6174de157b459aa33d6d3c9a9977b5a2b
Tannus Esquerdo <tannusewrton@gmail.com>
2022-10-02 21:22:21 -0400

    add projects page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
+var projectsRouter = require('./routes/projects');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
+app.use('/projects', projectsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
